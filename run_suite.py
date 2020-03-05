c52f55a4d1fcb3aaca3682f4a4a1c53f0e1b9f8a
Your Name <email@example.com>
2020-03-05 11:33:52 +0800

    123

--- run_suite.py
+++ run_suite.py
@@ -4,7 +4,8 @@ import os
 from HTMLTestRunner_PY3 import HTMLTestRunner
 
 suite = unittest.TestLoader().discover("./script",pattern="test*.py")
-report_path = os.path.dirname(os.path.abspath(__file__)) + "/report/ihrm{}.html".format(time.strftime("%Y%m%d %H%M%S"))
+# report_path = os.path.dirname(os.path.abspath(__file__)) + "/report/ihrm{}.html".format(time.strftime("%Y%m%d %H%M%S"))
+report_path = os.path.dirname(os.path.abspath(__file__)) + "/report/ihrm.html"
 with open(report_path,mode="wb") as f:
     runner = HTMLTestRunner(f,verbosity=2,title="ihrm系统登录和员工管理模块接口测试报告",description="Chrom80.0+Win10")
     runner.run(suite)
