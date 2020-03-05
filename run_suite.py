4d53a1d5b8c5df59ac3501f69e7ffd79f225d282
Your Name <email@example.com>
2020-03-05 11:56:17 +0800

    增加输出“helloworld”

--- run_suite.py
+++ run_suite.py
@@ -9,3 +9,5 @@ report_path = os.path.dirname(os.path.abspath(__file__)) + "/report/ihrm.html"
 with open(report_path,mode="wb") as f:
     runner = HTMLTestRunner(f,verbosity=2,title="ihrm系统登录和员工管理模块接口测试报告",description="Chrom80.0+Win10")
     runner.run(suite)
+
+print("Hello World")
